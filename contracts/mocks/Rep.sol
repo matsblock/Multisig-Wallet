856c4809eddb2abbee273a371162fcf25684fabc
matsblock <matsblock@outlook.com>
2022-03-22 13:08:19 -0300

    Smart contracts Working

---
+++ contracts/mocks/Rep.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract Rep is ERC20 {
+  constructor() ERC20('REP', 'Augur token')  {}
+
+  function faucet(address to, uint amount) external {
+    _mint(to, amount);
+  }
+}
